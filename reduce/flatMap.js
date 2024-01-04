5bd56a721cb834bc2f42ae98a561a61f30804052
roushan-sheik <roushansheik@gmail.com>
2024-01-04 08:05:17 +0600

    lets do it using flatmap

--- reduce/flatMap.js
+++ reduce/flatMap.js
@@ -1,3 +1,7 @@
 let arr = [1, 2, 3, 4, 5, 6];
 let result1 = arr.map((item) => [item * 2]).flat();
 console.log(result1);
+// flatMap
+let result2 = arr.flatMap((item) => [item * 2]);
+console.log(result2);
+// make it 2d arr
