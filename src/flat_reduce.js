af93bcda5646891b0f8a36837ae0fe8066c2d7d5
roushan-sheik <roushansheik@gmail.com>
2024-01-04 07:43:28 +0600

    make  one D array using reduce method

--- src/flat_reduce.js
+++ src/flat_reduce.js
@@ -2,3 +2,8 @@ let arr = [1, [2, 3], 4, 5, [6, 7, 8], 9, 10];
 // Lets make it one dimensional array using flat method
 let result = arr.flat();
 console.log(result);
+// well, now lets make it using reduce method
+let res = arr.reduce((acc, cur) => {
+  return acc.concat(cur);
+}, []);
+console.log(res);
