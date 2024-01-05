8221c64f0ef9a775ab8037bb7dd93c1932c7cbcf
roushan-sheik <roushansheik@gmail.com>
2024-01-05 21:45:06 +0600

    reduce implementation done

--- reduce/reduceImple.js
+++ reduce/reduceImple.js
@@ -1,19 +1,18 @@
 function myReduce(arr, cb, init) {
-  let acc = init;
   let start = 0;
+  let acc = init;
   if (!init) {
     acc = arr[0];
     start = 1;
   }
-
   for (let i = start; i < arr.length; i++) {
     acc = cb(acc, arr[i], i, arr);
   }
   return acc;
 }
-let arr = [1, 2, 3, 4, 5];
-const result = myReduce(arr, (acc, cur) => {
+// ================= Done =============
+let arr = [1, 2, 3, 4, 5, 6];
+let result = myReduce(arr, (acc, cur) => {
   return acc + cur;
 });
 console.log(result);
-// ====================== Done ======================
