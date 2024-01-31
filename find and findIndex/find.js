31768ff8e02b85913b212f94802e847b342b90ab
roushan-sheik <roushansheik@gmail.com>
2024-01-31 12:10:36 +0600

    find method behind the sense

---
+++ find and findIndex/find.js
@@ -0,0 +1,28 @@
+const orderBooks = [
+  { name: "javascript", price: 20 },
+  { name: "javascript", price: 50 },
+  { name: "Rust", price: 580 },
+  { name: "Python", price: 500 },
+  { name: "C", price: 400 },
+  { name: "C++", price: 400 },
+  { name: "Java", price: 600 },
+];
+//NOTE - Built in find() method
+let target_book = "C++";
+const result = orderBooks.find((book, indx, arr) => {
+  return book.name == "Rust";
+});
+console.log(result);
+//NOTE - Custom  find() method
+function myFind(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i], i, arr)) {
+      return arr[i];
+    }
+  }
+}
+
+const result2 = myFind(orderBooks, (book, indx) => {
+  return book.name == "C";
+});
+console.log(result2);
