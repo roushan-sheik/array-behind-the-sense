c027b7096563fc922ed0b264e8a0ff3b269f7b36
roushan-sheik <roushansheik@gmail.com>
2024-01-31 09:29:35 +0600

    Foreach Behind the sense

---
+++ Foreach/foreach.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, 3, 4, 5, 6];
+//NOTE - Builtin Foreach
+const result = arr.forEach((val, indx, arr) => {
+  console.log(val);
+});
+//NOTE - Foreach Behind the sense
+function myForeach(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i], i, arr);
+  }
+}
+myForeach(arr, (val, ind, arr) => {
+  console.log(val);
+});
