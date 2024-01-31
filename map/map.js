262396721d4a6c5d4a0d6f0d5ba7470ebe64217d
roushan-sheik <roushansheik@gmail.com>
2024-01-31 09:53:16 +0600

    map() Behind the sense added

--- map/map.js
+++ map/map.js
@@ -1,7 +1,20 @@
-let arr = [ 1, 2, 3, 4, 5, 6 ];
+let arr = [1, 2, 3, 4, 5, 6];
 //NOTE - Built in  map()
-arr.map( ( val, ind, arr ) =>
-{
-    console.log(val)
-} )
+arr.map((val, ind, arr) => {
+  console.log(val);
+});
+//NOTE - map() Behind the sense
+function myMap(arr, cb) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let temp = cb(arr[i], i, arr);
+    newArr.push(temp);
+  }
+  return newArr;
+}
+// Testing our custom map()
 
+const sqrResult = myMap(arr, (val, indx, arr) => {
+  return (val *= val);
+});
+console.log(sqrResult);
